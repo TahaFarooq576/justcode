a4af54f38e6d7c55e9757863ff26b9cb827f37a4
TahaFarooq576 <tahafarooq576@gmail.com>
2025-07-06 00:02:02 -0700

    javascript undrstanding

---
+++ javascript/script.js
@@ -0,0 +1,22 @@
+console.log("hello world");
+console.log("hello web world");
+console.log("Welcome to javascript");
+
+
+var a=prompt("Enter aa number");
+console.log("You entered "+a);
+
+
+var istrue=confirm("you want to leave this page and burn your computer");
+
+if(istrue){
+    console.log("Your computer is burned");
+}
+else{
+    console.log("Your computer is not burned");
+}
+
+
+// document.title= "First javascript code";
+document.body.style.backgroundColor = "red";
+document.title= "First javascript code";
