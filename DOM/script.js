949631b35e2038f86d221ac4a162f7d23fea8dc2
TahaFarooq576 <tahafarooq576@gmail.com>
2025-07-09 02:30:56 -0700

    Dom mainupulation

---
+++ DOM/script.js
@@ -0,0 +1,18 @@
+document.getElementById("color").style.backgroundColor="yellow";
+
+
+let a=document.getElementsByClassName("box");
+a[3].style.backgroundColor="pink";
+
+
+let b=document.getElementsByClassName("box")
+a[2].style.color="blue";
+
+
+document.querySelector(".box")
+.style.backgroundColor="blue"
+
+let c=document.querySelectorAll(".box");
+c.forEach(e=>{
+e.style.backgroundColor="yellow"
+})
