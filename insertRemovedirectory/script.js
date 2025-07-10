4d429868bf7396426a92c367d36028a4dbbbb4e0
TahaFarooq576 <tahafarooq576@gmail.com>
2025-07-10 10:30:41 -0700

    Inertion&Deletion Dom

---
+++ insertRemovedirectory/script.js
@@ -0,0 +1,11 @@
+
+
+document.getElementsByClassName("box1")[0].style.backgroundColor="orange";
+document.getElementById("box2").style.color="pink";
+
+document.querySelector(".box3").style.backgroundColor="purple";
+
+ let a=document.querySelectorAll(".box4");
+ a.forEach(e=>{
+    e.style.backgroundColor="green"
+ })
