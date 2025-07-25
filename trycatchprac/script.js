a41979c68a921e5db6b3edb2819b52c23902d942
TahaFarooq576 <tahafarooq576@gmail.com>
2025-07-25 08:41:02 -0700

    try catch practice

---
+++ trycatchprac/script.js
@@ -0,0 +1,44 @@
+let a=prompt("Enter a number");
+let b=prompt("Enter Second number");
+let sum;
+
+sum=parseInt(a)+parseInt(b);
+console.log("Your sum is=",sum);
+
+
+console.log("Your sum=",sum*2);
+
+try {
+    console.log("Your sum=",sum*e);
+} catch (error) {
+    console.log("First declare unknown variable then perform calculation");
+}
+
+finally{
+    console.log("All files are closed you can correct mistake and then perform calculation thanks!")
+}
+
+
+let m=prompt("Enter a number");
+let t=prompt("Enter Second number");
+let uum;
+
+uum=parseInt(m)+parseInt(t);
+console.log("Your sum is=",uum);
+
+
+console.log("Your sum=",uum*2);
+
+try {
+    console.log("Your sum=",uum*e);
+} catch (error) {
+    console.log("First declare unknown variable then perform calculation");
+}
+
+finally{
+    console.log("All files are closed you can correct mistake and then perform calculation thanks!")
+}
+
+
+
+
