9ec9f606a6ebfc8784861d82e4e1dd5d3d04eea6
TahaFarooq576 <tahafarooq576@gmail.com>
2025-07-26 11:40:55 -0700

    DOM Mainupulation

---
+++ dommain/script.js
@@ -0,0 +1,20 @@
+
+function getRandomColor() {
+    let a = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    let c = Math.floor(Math.random() * 255);
+
+    return `rgb(${a}, ${b}, ${c})`;
+}
+
+let button = document.querySelector(".btn");
+
+button.addEventListener("click", function () {
+    button.style.backgroundColor = getRandomColor();
+    button.style.borderRadius = "10px";
+});
+
+setInterval(() => {
+    button.style.backgroundColor = getRandomColor();
+    button.style.borderRadius = "10px";
+}, 10000);
